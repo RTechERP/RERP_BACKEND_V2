3d830975b6bc9a8fa0a12272cd0a903d0970c6b6
nhubinh2104 <92156609+Binh2104@users.noreply.github.com>
2025-06-13 08:14:27 +0700

    create: them sua xoa nhom kho vat tu

---
+++ RERPAPI.Repo/GenericEntity/ProductsSaleRepo.cs
@@ -0,0 +1,16 @@
+﻿using RERPAPI.Model.Common;
+using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Repo.GenericEntity
+{
+    public class ProductsSaleRepo: GenericRepo<ProductSale>
+    {
+       
+
+    }
+}
