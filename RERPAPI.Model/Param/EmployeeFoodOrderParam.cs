80eac71ae62e1c416e481cd9991030bbc41a346c
xu1112003 <phungchien30031993@gmail.com>
2025-07-09 14:59:55 +0700

    Thêm API chức năng Cơm ca

---
+++ RERPAPI.Model/Param/EmployeeFoodOrderParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param
+{
+    public class EmployeeFoodOrderParam
+    {
+        public DateTime dateStart { get; set; }
+        public DateTime dateEnd { get; set; }
+        public string keyWord { get; set; } = "";
+        public int employeeId { get; set; } = 0;
+        public int pageNumber { get; set; } = 1;
+        public int pageSize { get; set; } = 1000000;
+
+    }
+}
