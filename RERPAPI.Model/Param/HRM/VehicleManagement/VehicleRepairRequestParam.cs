dec815870adf6bbf5f9949d1d77a42daee0b46d1
luongxuan1406 <luongtu1112@gmail.com>
2025-10-20 08:33:38 +0700

    Update API danh sách xe, lịch sử sửa chữa, loại sửa chữa

---
+++ RERPAPI.Model/Param/HRM/VehicleManagement/VehicleRepairRequestParam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param.HRM.VehicleManagement
+{
+    public  class VehicleRepairRequestParam
+    {
+        public int Page { get; set; } = 1;
+        public int Size { get; set; } = 99999;
+        public DateTime? @DateStart { get; set; } 
+        public DateTime? DateEnd { get; set; }
+        public string FilterText { get; set; } = "";
+        public int EmployeeID { get; set; } = 0;
+        public int VehicleID { get; set; } = 0;
+        public int TypeID { get; set; } = 0;
+    }
+}
---
+++ RERPAPI.Repo/GenericEntity/HRM/Vehicle/VehicleRepairRepo.cs
@@ -0,0 +1,51 @@
+﻿using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Repo.GenericEntity.HRM.Vehicle
+{
+    public class VehicleRepairRepo:GenericRepo<VehicleRepair>
+    {
+        public bool Validate(VehicleRepair item, out string message)
+        {
+            message = "";
+
+            if (!string.IsNullOrEmpty(item.Reason) == false)
+            {
+                message = "Vui lòng nhập lí do sửa chữa";
+                return false;
+            }
+
+            if (item.VehicleRepairTypeID != null && item.VehicleRepairTypeID <= 0)
+            {
+                message = "Vui lòng chọn kiểu sửa chữa";
+                return false;
+            }
+
+            if (item.TimeStartRepair != null && !item.TimeStartRepair.HasValue)
+            {
+                message = "Vui lòng nhập thời gian bắt đầu sửa chữa";
+                return false;
+            }
+            if (item.DateReport != null && !item.DateReport.HasValue)
+
+            {
+                message = "Vui lòng chọn ngày báo cáo sửa chữa";
+                return false;
+            }
+         
+
+            if (item.CostRepairEstimate != null && !item.CostRepairEstimate.HasValue)
+            {
+                message = "Vui lòng nhập chi phí ước tính";
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
