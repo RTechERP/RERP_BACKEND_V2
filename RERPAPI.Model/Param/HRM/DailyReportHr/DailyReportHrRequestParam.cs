0f667ae866caf156fcc172f871dd4ff54c4cfa06
luongxuan1406 <luongtu1112@gmail.com>
2025-11-05 19:38:09 +0700

    Commit be  chức năng đăng kí VPP, tổng hợp VPP

---
+++ RERPAPI.Model/Param/HRM/DailyReportHr/DailyReportHrRequestParam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param.HRM.DailyReportHr
+{
+    public class DailyReportHrRequestParam
+    {
+
+        public DateTime? dateStart { get; set; }      // cho nullable
+        public DateTime? dateEnd { get; set; }        // cho nullable
+        public string keyword { get; set; } = "";
+        public int departmentID { get; set; } = 6;  
+        public int userID { get; set; } = 0;
+        public int employeeID { get; set; } = 0;
+
+    }
+}
