e8e3df3c8494015c5c4603c96bcd8812627b6723
luongxuan1406 <luongtu1112@gmail.com>
2025-11-13 10:50:10 +0700

    Commit be chức năng quản lí tài sản

--- RERPAPI.Model/Common/RoleConfig.cs
+++ RERPAPI.Model/Common/RoleConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RERPAPI.Model.DTO
+namespace RERPAPI.Model.Common
 {
     public class RoleConfig
     {
--- RERPAPI/Program.cs
+++ RERPAPI/Program.cs
@@ -14,6 +14,7 @@ using RERPAPI.Repo.GenericEntity.AddNewBillExport;
 using RERPAPI.Repo.GenericEntity.Asset;
 using RERPAPI.Repo.GenericEntity.BBNV;
 using RERPAPI.Repo.GenericEntity.Duan.MeetingMinutes;
+using RERPAPI.Repo.GenericEntity.Film;
 using RERPAPI.Repo.GenericEntity.HRM;
 using RERPAPI.Repo.GenericEntity.HRM.Vehicle;
 using RERPAPI.Repo.GenericEntity.Project;
@@ -36,6 +37,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IUserPermissionService, UserPermissionService>();
 builder.Services.AddScoped<RTCContext>();
+builder.Services.AddScoped<RoleConfig>();
+
 builder.Services.AddScoped<EmployeeOnLeaveRepo>();
 builder.Services.AddScoped<RERPAPI.Repo.GenericEntity.AddressStockRepo>();
 builder.Services.AddScoped<BillDocumentExportLogRepo>();
@@ -220,6 +223,10 @@ builder.Services.AddScoped<ProductsRepo>();
 
 
 builder.Services.AddScoped<TSAllocationAssetPersonalDetailRepo>();
+builder.Services.AddScoped<FilmManagementRepo>();
+
+builder.Services.AddScoped<FilmManagementDetailRepo>();
+
 builder.Services.AddScoped<TSAllocationAssetPersonalRepo>();
 builder.Services.AddScoped<TSAllocationEvictionAssetRepo>();
 builder.Services.AddScoped<TSAssetAllocationDetailRepo>();
@@ -395,7 +402,14 @@ builder.Services.AddRouting(options =>
     options.LowercaseUrls = true; // Chuyển tất cả URL thành chữ thường
 });
 
+var roleConfigSection = builder.Configuration.GetSection("RoleConfig");
 
+// Bind vào IOptions<RoleConfig>
+builder.Services.Configure<RoleConfig>(roleConfigSection);
+
+// Đăng ký singleton để inject trực tiếp RoleConfig
+builder.Services.AddSingleton(sp =>
+    sp.GetRequiredService<IOptions<RoleConfig>>().Value);
 builder.Services.Configure<ModulaConfig>(builder.Configuration.GetSection("ModulaConfig"));
 
 // Nếu bạn muốn inject trực tiếp:
@@ -450,11 +464,11 @@ app.UseStaticFiles();
 List<PathStaticFile> staticFiles = builder.Configuration.GetSection("PathStaticFiles").Get<List<PathStaticFile>>() ?? new List<PathStaticFile>();
 foreach (var item in staticFiles)
 {
-    app.UseStaticFiles(new StaticFileOptions()
-    {
-        FileProvider = new PhysicalFileProvider($@"\\192.168.1.190\Software"),
-        RequestPath = new PathString($"/api/share/{item.PathName.Trim().ToLower()}")
-    });
+    //app.UseStaticFiles(new StaticFileOptions()
+    //{
+    //    FileProvider = new PhysicalFileProvider($@"\\192.168.1.190\Software"),
+    //    RequestPath = new PathString($"/api/share/{item.PathName.Trim().ToLower()}")
+    //});
 
 
     //app.UseDirectoryBrowser(new DirectoryBrowserOptions
