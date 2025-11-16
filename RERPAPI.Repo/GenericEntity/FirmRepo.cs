8aa8df5c8771af8b1372a2484d8168ceea698228
Nhu Binh <Nhu Binh@DESKTOP-7O4JCTA>
2025-11-16 23:59:13 +0700

    fix commit merge master

--- RERPAPI.Repo/GenericEntity/FirmRepo.cs
+++ RERPAPI.Repo/GenericEntity/FirmRepo.cs
@@ -15,7 +15,6 @@ namespace RERPAPI.Repo.GenericEntity
             {
 
                 var query = GetAll(f => (f.FirmCode ?? "").ToUpper() == firmCode.ToUpper() && f.IsDelete != true);
-                var query = GetAll(f => f.FirmCode.ToUpper() == firmCode.ToUpper() && f.IsDelete != true);
 
                 if (id.HasValue)
                 {
