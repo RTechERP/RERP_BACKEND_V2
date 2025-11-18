4fdb5df2d95f3f04721c110834afd2f293e7c3b1
Admin <Admin@DESKTOP-40H717B>
2025-11-18 19:19:56 +0700

    - Update Lịch sử mượn agv

---
+++ RERPAPI.Repo/GenericEntity/Warehouses/AGV/AGVInventoryDemoRepo.cs
@@ -0,0 +1,30 @@
+﻿using RERPAPI.Model.DTO;
+using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Repo.GenericEntity.Warehouses.AGV
+{
+    public class AGVInventoryDemoRepo : GenericRepo<AGVInventoryDemo>
+    {
+        public AGVInventoryDemoRepo(CurrentUser currentUser) : base(currentUser)
+        {
+        }
+
+
+        public void SaveData(AGVInventoryDemo inventory)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
