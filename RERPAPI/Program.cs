fe3e747e42fa362877f7d44f363c14565aab1efd
Admin <Admin@DESKTOP-40H717B>
2025-11-22 10:59:07 +0700

    - Sửa config static file

--- RERPAPI/Program.cs
+++ RERPAPI/Program.cs
@@ -516,12 +516,12 @@ app.UseStaticFiles();
 List<PathStaticFile> staticFiles = builder.Configuration.GetSection("PathStaticFiles").Get<List<PathStaticFile>>() ?? new List<PathStaticFile>();
 foreach (var item in staticFiles)
 {
-   /* app.UseStaticFiles(new StaticFileOptions()
+    app.UseStaticFiles(new StaticFileOptions()
     {
         FileProvider = new PhysicalFileProvider(item.PathFull),
         RequestPath = new PathString($"/api/share/{item.PathName.Trim().ToLower()}")
     });
-*/
+
 
     app.UseDirectoryBrowser(new DirectoryBrowserOptions
     {
