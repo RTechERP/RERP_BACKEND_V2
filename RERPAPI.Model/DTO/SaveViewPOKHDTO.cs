3cadfe1e68e4058cc910294db56c2d5b4d31bee3
tuanbeo <anhemnha8x@gmail.com>
2025-11-26 17:46:51 +0700

    update tổng hợp báo cáo, fix code merge của Mr 5per, update save data viewpokh

---
+++ RERPAPI.Model/DTO/SaveViewPOKHDTO.cs
@@ -0,0 +1,15 @@
+﻿using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.DTO
+{
+    public class SaveViewPOKHDTO
+    {
+        public List<POKHDetail> pokhDetails { get; set; }
+        public List<RequestInvoiceDetail> requestInvoiceDetails { get; set; }
+    }
+}
