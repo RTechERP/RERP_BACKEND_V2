57b8b539ef6177cd4ab8aca4847af4cb5c8fd79f
luongxuan1406 <luongtu1112@gmail.com>
2025-12-01 10:46:04 +0700

    Commit be chức năng tổng hợp PO NCC

---
+++ RERPAPI.Model/Param/PONCCSummaryRequestParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param
+{
+    public class PONCCSummaryRequestParam
+    {
+        public DateTime? @DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+        public string FilterText { get; set; } = "";
+
+        public int SupplierID { get; set; }
+        public int Status { get; set; }
+        public int EmployeeID { get; set; }
+    }
+}
