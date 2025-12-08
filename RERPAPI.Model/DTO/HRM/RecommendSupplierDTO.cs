dd3eaa30173caf98701949f613835e390d67270a
ADMIN <ADMIN@DESKTOP-I8MS0CC>
2025-12-08 15:04:36 +0700

    Tạo đề xuất của HR

---
+++ RERPAPI.Model/DTO/HRM/RecommendSupplierDTO.cs
@@ -0,0 +1,20 @@
+﻿using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.DTO.HRM
+{
+    public class RecommendSupplierDTO
+    {
+        //public JobRequirement JobRequirement { get; set; }
+        public int JobRequirementID { get; set; }
+        public List<DepartmentRequired>? DepartmentRequired { get; set; }
+        public List<HCNSProposal>? HCNSProposal { get; set; }
+        public List<int>? DeletedCommend { get; set; }
+        //public List<DepartmentRequiredApproval>? DepartmentRequiredApproval { get; set; }
+
+    }
+}
