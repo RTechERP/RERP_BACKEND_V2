3d907cfd501d910cf9ab714a057b5ac2fe1b10e3
luongxuan1406 <luongtu1112@gmail.com>
2025-12-08 17:25:34 +0700

    Commit sửa chức năng tồn kho theo sản phẩm, API lấy danh sách để duyệt

---
+++ RERPAPI.Model/Param/ApproveByApproveTPRequestParam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param
+{
+    public class ApproveByApproveTPRequestParam
+    {
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+        public string? FilterText { get; set; }
+        public int? IDApprovedTP { get; set; }
+        public int? Status { get; set; }
+        public int? DeleteFlag { get; set; }
+        public int? EmployeeID { get; set; }
+        public int? TType { get; set; }
+        public int? StatusHR { get; set; }
+        public int? StatusBGD { get; set; }
+        public int? IsBGD { get; set; }
+        public int? UserTeamID { get; set; }
+
+    }
+}
