1d42ce96ff02037d2d4ee8e41cea8a35baa2df02
luongxuan1406 <luongtu1112@gmail.com>
2025-12-10 08:59:05 +0700

    commit pending phần duyệt để đẩy form đăng ký

---
+++ RERPAPI.Model/DTO/ApproveTPDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.DTO
+{
+    public class ApproveTPDTO
+    {
+        public class ApproveItemParam
+        {
+            public int? Id { get; set; }
+            public string? TableName { get; set; } = "";
+            public string? ValueUpdatedDate { get; set; } = "";
+            public string? ValueDecilineApprove { get; set; } = "";
+            public string? EvaluateResults { get; set; } = "";
+            public string? FullName { get; set; } = "";
+            public string? FieldName { get; set; } = "";
+
+            public bool? DeleteFlag { get; set; }
+            public bool? IsApprovedTP { get; set; }
+            public bool? IsApprovedHR { get; set; }
+            public bool? IsApprovedBGD { get; set; }
+            public int? IsCancelRegister { get; set; }
+            public int? TType { get; set; }           
+
+            public bool? IsSeniorApproved { get; set; } 
+        }
+
+        public class ApproveRequestParam
+        {
+            public bool? IsApproved { get; set; }               
+            public List<ApproveItemParam>? Items { get; set; } = new();
+        }
+
+        public class NotProcessedApprovalItem
+        {
+            public ApproveItemParam? Item { get; set; } = new();
+            public string? Reason { get; set; } = "";
+        }
+    }
+}
