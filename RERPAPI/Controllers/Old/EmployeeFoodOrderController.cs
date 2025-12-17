4e499fa54d5134eb747099c0d409f508228ef02c
luongxuan1406 <luongtu1112@gmail.com>
2025-12-17 09:00:55 +0700

    Update validate đăng ký đặt cơm

--- RERPAPI/Controllers/Old/EmployeeFoodOrderController.cs
+++ RERPAPI/Controllers/Old/EmployeeFoodOrderController.cs
@@ -160,9 +160,11 @@ namespace RERPAPI.Controllers.Old
         {
             try
             {
+                var now = DateTime.Now;
                 var claims = User.Claims.ToDictionary(x => x.Type, x => x.Value);
                 CurrentUser currentUser = ObjectMapper.GetCurrentUser(claims);
-
+                var today = now.Date;
+                var yesterday = today.AddDays(-1);
                 // Các quyền được phép thao tác theo EmployeeID tùy ý
                 string[] allowCodes = new[] { "N23", "N1", "N2", "N34", "N80" };
 
@@ -170,7 +172,7 @@ namespace RERPAPI.Controllers.Old
                 var vUserHR = _vUserGroupLinksRepo
                     .GetAll()
                     .FirstOrDefault(x =>
-          (x.Code == "N23" || x.Code == "N1" || x.Code == "N2" || x.Code == "N34" || x.Code == "N80") &&
+          (x.Code == "N23" || x.Code == "N1" || x.Code == "N2" || x.Code == "N34") &&
           x.UserID == currentUser.ID);
 
                 int employeeID;
@@ -183,7 +185,7 @@ namespace RERPAPI.Controllers.Old
                 {
                     employeeID = currentUser.EmployeeID;
                 }
-                var now = DateTime.Now;
+             
                 var today10AM = new DateTime(now.Year, now.Month, now.Day, 10, 0, 0);
 
                 if (vUserHR == null && now > today10AM)
@@ -206,9 +208,48 @@ namespace RERPAPI.Controllers.Old
                     foodOrder.ID = checkExist.ID;
                     foodOrder.Quantity = foodOrder.Quantity + checkExist.Quantity;
                 }
-
+                if (foodOrder.IsDeleted == true && vUserHR == null && foodOrder.DateOrder.HasValue)
+                {
+                    var orderDate = foodOrder.DateOrder.Value.Date;
+                    if (orderDate < today)
+                    {
+                        return BadRequest(ApiResponseFactory.Fail(
+                            null,
+                            "Không thể xoá phiếu đặt cơm của các ngày trước."
+                        ));
+                    }
+                    if (orderDate == today && now > today10AM)
+                    {
+                        return BadRequest(ApiResponseFactory.Fail(
+                            null,
+                            "Sau 10h không thể xoá phiếu đặt cơm của ngày hôm nay."
+                        ));
+                    }
+                }
                 if (foodOrder.ID <= 0) await _employeeFoodOrderRepo.CreateAsync(foodOrder);
-                else await _employeeFoodOrderRepo.UpdateAsync(foodOrder);
+                else
+                {
+                    if(foodOrder.ID >0 && vUserHR == null && foodOrder.DateOrder.HasValue)
+                    {
+                        var orderDate = foodOrder.DateOrder.Value.Date;
+                        if (orderDate < today)
+                        {
+                            return BadRequest(ApiResponseFactory.Fail(
+                                null,
+                                "Không thể sửa đặt cơm của ngày hôm trước."
+                            ));
+                        }
+                        if (orderDate == today && now > today10AM)
+                        {
+                            return BadRequest(ApiResponseFactory.Fail(
+                                null,
+                                "Không thể sửa đặt cơm sau 10H ngày hôm nay."
+                            ));
+                        }
+                    }    
+
+                    await _employeeFoodOrderRepo.UpdateAsync(foodOrder);
+                }
                 return Ok(ApiResponseFactory.Success(foodOrder, "Lưu thành công"));
             }
             catch (Exception ex)
