57d65188ffaa37fdc77f7729f28e26284fa0c9af
luongxuan1406 <luongtu1112@gmail.com>
2025-12-26 09:09:40 +0700

    Update tài sản cá nhân, kế hoạch công việc

---
+++ RERPAPI.Model/DTO/WorkPlanDetailDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.DTO
+{
+    public class WorkPlanDetailDTO
+    {
+        public string FullName { get; set; }
+        public string Day1 { get; set; }
+        public string Day2 { get; set; }
+        public string Day3 { get; set; }
+        public string Day4 { get; set; }
+        public string Day5 { get; set; }
+        public string Day6 { get; set; }
+        public string Day7 { get; set; }
+        public string WorkPlanPreviousWeek { get; set; }
+        public string WorkPlanCurrentWeek { get; set; }
+
+        public class WorkPlanPreCur
+        {
+            public int UserID { get; set; }
+            public string WorkContentPre { get; set; }
+            public string WorkContentCur { get; set; }
+        }
+    }
+}
---
+++ RERPAPI.Model/Param/WorkPlanSummaryParam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.Param
+{
+    public class WorkPlanSummaryParam
+    {
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+        public string? Keyword { get; set; }
+        public int DepartmentId { get; set; }
+        public int TeamId { get; set; }
+        public int Type { get; set; } // 0: Tổng hợp, 1: Chi tiết
+        public int UserId { get; set; }
+    }
+}
