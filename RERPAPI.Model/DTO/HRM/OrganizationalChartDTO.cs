225db5668a881922643880a23bb645745d84b4ee
luongxuan1406 <luongtu1112@gmail.com>
2025-12-27 14:15:14 +0700

    Commit be chức năng sơ đồ tổ chức

---
+++ RERPAPI.Model/DTO/HRM/OrganizationalChartDTO.cs
@@ -0,0 +1,15 @@
+﻿using RERPAPI.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Model.DTO.HRM
+{
+    public class OrganizationalChartDTO
+    {
+        public List<OrganizationalChart>? organizationalCharts { get; set; }
+        public List<OrganizationalChartDetail>? organizationalChartDetails { get; set; }
+    }
+}
