d1697f21f14ff3c4c215607b75cbd6ab25334b2f
rtcnhat18 <tech61@rtc.edu.vn>
2025-12-28 23:36:36 +0700

    update tổng hợp phiếu nhập

---
+++ RERPAPI.Model/Param/Technical/BillImportTechnicalSummaryParam.cs
@@ -0,0 +1,18 @@
+﻿namespace RERPAPI.Model.Param.Technical
+{
+    public class BillImportTechnicalSummaryParam
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+
+        public bool IsAll { get; set; }
+
+        public int Status { get; set; }   // = Status trong SP
+        public string? FilterText { get; set; }
+
+        public int WarehouseId { get; set; }
+    }
+}
