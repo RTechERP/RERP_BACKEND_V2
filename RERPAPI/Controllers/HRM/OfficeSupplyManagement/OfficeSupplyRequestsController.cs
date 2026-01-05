fd922ae83d6a078b9a316066b126d0b2a7d89881
luongxuan1406 <luongtu1112@gmail.com>
2026-01-05 11:49:46 +0700

    Update api xem VPP

--- RERPAPI/Controllers/HRM/OfficeSupplyManagement/OfficeSupplyRequestsController.cs
+++ RERPAPI/Controllers/HRM/OfficeSupplyManagement/OfficeSupplyRequestsController.cs
@@ -17,7 +17,7 @@ namespace RERPAPI.Controllers.HRM.OfficeSupplyManagement
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize] 
+    [Authorize]
     public class OfficeSupplyRequestsController : ControllerBase
     {
         private readonly OfficeSupplyRequestsRepo officesupplyrequests;
@@ -42,20 +42,29 @@ namespace RERPAPI.Controllers.HRM.OfficeSupplyManagement
         {
             try
             {
-                //List<Department> departmentList = SQLHelper<Department>.FindAll().OrderBy(x => x.STT).ToList();
-                List<Model.Entities.Department> departmentList = _departmentRepo.GetAll().OrderBy(x => x.STT).ToList();
-                return Ok(ApiResponseFactory.Success(departmentList, ""));
+                var claims = User.Claims.ToDictionary(x => x.Type, x => x.Value);
+                CurrentUser currentUser = ObjectMapper.GetCurrentUser(claims);
+
+                var departments = _departmentRepo
+                    .GetAll()
+                    .OrderBy(x => x.STT)
+                    .ToList();
+                if (currentUser.EmployeeID == 331 || currentUser.EmployeeID == 548 || currentUser.EmployeeID == 668)
+                {
+                    var departmentIDs = new List<int> { 9, 10, currentUser.DepartmentID };
+                    departments = departments
+                        .Where(x => departmentIDs.Contains(x.ID))
+                        .ToList();
+                }
+
+                return Ok(ApiResponseFactory.Success(departments, ""));
             }
             catch (Exception ex)
             {
-                return Ok(new
-                {
-                    status = 0,
-                    message = ex.Message,
-                    error = ex.ToString()
-                });
+                return BadRequest(ApiResponseFactory.Fail(ex, ex.Message));
             }
         }
+
 
         /// <summary>
         /// hàm lấy dữ liệu danh sách đăng ký VPP
@@ -65,70 +74,42 @@ namespace RERPAPI.Controllers.HRM.OfficeSupplyManagement
         /// <param id phòng ban đăng ký="departmentID"></param>
         /// <param tháng="monthInput"></param>
         /// <returns></returns>
-
         [HttpGet("get-office-supply-request")]
-        public IActionResult GetOfficeSupplyRequests(
-        string? keyword,
-        int? employeeID,
-        int? departmentID,
-        DateTime? monthInput)
+        public IActionResult GetOfficeSupplyRequests(string? keyword,DateTime? monthInput, int? departmentID) 
         {
             try
             {
-                // Lấy currentUser từ claims
                 var claims = User.Claims.ToDictionary(x => x.Type, x => x.Value);
                 CurrentUser currentUser = ObjectMapper.GetCurrentUser(claims);
 
-                keyword ??= string.Empty;
+                string key = keyword?.Trim() ?? string.Empty;
                 DateTime month = monthInput ?? new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
 
-                bool isPowerUser =
-                    (_roleConfig.UserAllsOfficeSupply?.Contains(currentUser.EmployeeID) ?? false) ||
-                    (_roleConfig.TBPEmployeeIds?.Contains(currentUser.EmployeeID) ?? false) ||
-                    (_roleConfig.PBPPositionCodes?.Contains(currentUser.PositionCode) ?? false) ||
-                    currentUser.IsAdmin==true; // nếu có cờ IsAdmin
-                bool xemAll = (_roleConfig.UserAllsOfficeSupply?.Contains(currentUser.EmployeeID) ?? false);
                 int effectiveDepartmentId;
-                int effectiveEmployeeId;
-
-                if (isPowerUser)
+                int effectiveEmployeeId = 0;
+                if (_roleConfig.UserAllsOfficeSupply?.Contains(currentUser.EmployeeID) == true)
                 {
-
-                    if (xemAll)
-                    {
-                        effectiveDepartmentId = 0;
-                    }
-                    else
-                    {
-                        effectiveDepartmentId = currentUser.DepartmentID; // 0 = all trong SP
-                    }
-                    effectiveEmployeeId = employeeID ?? 0;     // 0 = all trong SP
+                    effectiveDepartmentId = departmentID ?? 0;
+                }
+                if (currentUser.EmployeeID == 331 || currentUser.EmployeeID == 548 || currentUser.EmployeeID == 668)
+                {
+                    effectiveDepartmentId = departmentID??2;
                 }
                 else
                 {
-                    // Nhân viên thường:
-                    // - Dept: cố định theo phòng ban
-                    // - Chỉ xem được phiếu do nó đăng ký
-                    effectiveDepartmentId = currentUser.DepartmentID;
                     effectiveEmployeeId = currentUser.EmployeeID;
-                }
-                if (currentUser.EmployeeID == 331 || currentUser.EmployeeID == 548) //nếu là hương và Hà admin
-                {
+
                     effectiveDepartmentId = currentUser.DepartmentID;
-                    effectiveEmployeeId = 0;
                 }
                 List<List<dynamic>> result = SQLHelper<dynamic>.ProcedureToList(
                     "spGetOfficeSupplyRequests",
-                    new string[] { "@KeyWord", "@MonthInput", "@EmployeeID", "@DepartmentID" },
-                    new object[] { keyword, month, effectiveEmployeeId, effectiveDepartmentId }
+                    new[] { "@MonthInput", "@KeyWord", "@EmployeeID", "@DepartmentID" },
+                    new object[] { month, key, effectiveEmployeeId, effectiveDepartmentId }
                 );
-
-                List<dynamic> rs = result.Count > 0 ? result[0] : new List<dynamic>();
-
                 return Ok(new
                 {
                     status = 1,
-                    data = rs
+                    data = result.Count > 0 ? result[0] : new List<dynamic>()
                 });
             }
             catch (Exception ex)
@@ -141,6 +122,82 @@ namespace RERPAPI.Controllers.HRM.OfficeSupplyManagement
                 });
             }
         }
+
+        //[HttpGet("get-office-supply-request")]
+        //public IActionResult GetOfficeSupplyRequests(
+        //string? keyword,
+        //int? employeeID,
+        //int? departmentID,
+        //DateTime? monthInput)
+        //{
+        //    try
+        //    {
+        //        // Lấy currentUser từ claims
+        //        var claims = User.Claims.ToDictionary(x => x.Type, x => x.Value);
+        //        CurrentUser currentUser = ObjectMapper.GetCurrentUser(claims);
+
+        //        keyword ??= string.Empty;
+        //        DateTime month = monthInput ?? new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+
+        //        bool isPowerUser =
+        //            (_roleConfig.UserAllsOfficeSupply?.Contains(currentUser.EmployeeID) ?? false) ||
+        //            (_roleConfig.TBPEmployeeIds?.Contains(currentUser.EmployeeID) ?? false) ||
+        //            (_roleConfig.PBPPositionCodes?.Contains(currentUser.PositionCode) ?? false) ||
+        //            currentUser.IsAdmin==true; // nếu có cờ IsAdmin
+        //        bool xemAll = (_roleConfig.UserAllsOfficeSupply?.Contains(currentUser.EmployeeID) ?? false);
+        //        int effectiveDepartmentId;
+        //        int effectiveEmployeeId;
+
+        //        if (isPowerUser)
+        //        {
+
+        //            if (xemAll)
+        //            {
+        //                effectiveDepartmentId = 0;
+        //            }
+        //            else
+        //            {
+        //                effectiveDepartmentId = currentUser.DepartmentID; // 0 = all trong SP
+        //            }
+        //            effectiveEmployeeId = employeeID ?? 0;     // 0 = all trong SP
+        //        }
+        //        else
+        //        {
+        //            // Nhân viên thường:
+        //            // - Dept: cố định theo phòng ban
+        //            // - Chỉ xem được phiếu do nó đăng ký
+        //            effectiveDepartmentId = currentUser.DepartmentID;
+        //            effectiveEmployeeId = currentUser.EmployeeID;
+        //        }
+        //        if (currentUser.EmployeeID == 331 || currentUser.EmployeeID == 548) //nếu là hương và Hà admin
+        //        {
+        //            effectiveDepartmentId = currentUser.DepartmentID;
+        //            effectiveEmployeeId = 0;
+        //        }
+        //        List<List<dynamic>> result = SQLHelper<dynamic>.ProcedureToList(
+        //            "spGetOfficeSupplyRequests",
+        //            new string[] { "@KeyWord", "@MonthInput", "@EmployeeID", "@DepartmentID" },
+        //            new object[] { keyword, month, effectiveEmployeeId, effectiveDepartmentId }
+        //        );
+
+        //        List<dynamic> rs = result.Count > 0 ? result[0] : new List<dynamic>();
+
+        //        return Ok(new
+        //        {
+        //            status = 1,
+        //            data = rs
+        //        });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Ok(new
+        //        {
+        //            status = 0,
+        //            message = ex.Message,
+        //            error = ex.ToString()
+        //        });
+        //    }
+        //}
 
         /// <summary>
         /// Hàm lấy chi tiết đăng ký văn phòng phẩm
@@ -357,16 +414,16 @@ namespace RERPAPI.Controllers.HRM.OfficeSupplyManagement
         [RequiresPermission("N2,N34,N1,N54,N72")]
         [HttpPost("save-data")]
         public async Task<IActionResult> SaveData([FromBody] OfficeSupplyRequestDTO dto)
-            {
+        {
             try
             {
-             
+
                 if (dto == null) { return BadRequest(new { status = 0, message = "Dữ liệu gửi lên không hợp lệ." }); }
-               
+
                 if (dto.officeSupplyRequest != null)
                 {
                     var validate = officesupplyrequests.Validate(dto.officeSupplyRequest);
-                    if (validate.status == 0) return BadRequest(validate);  
+                    if (validate.status == 0) return BadRequest(validate);
                     if (dto.officeSupplyRequest.ID <= 0)
                         await officesupplyrequests.CreateAsync(dto.officeSupplyRequest);
                     else
