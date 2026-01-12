7ffffe8f90ed1a17e02c1d5a1f090a8263716496
luongxuan1406 <luongtu1112@gmail.com>
2026-01-12 14:27:27 +0700

    Commit sửa param mặc định

--- RERPAPI.Model/Param/ApproveByApproveTPRequestParam.cs
+++ RERPAPI.Model/Param/ApproveByApproveTPRequestParam.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RERPAPI.Model.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ namespace RERPAPI.Model.Param
 {
     public class ApproveByApproveTPRequestParam
     {
-        public DateTime? DateStart { get; set; } = DateTime.MinValue;
-        public DateTime? DateEnd { get; set; } = DateTime.MaxValue;
+        public DateTime? DateStart { get; set; } = TextUtils.MinDate;
+        public DateTime? DateEnd { get; set; } = TextUtils.MaxDate;
         public string? FilterText { get; set; } = "";
         public int? IDApprovedTP { get; set; } = 0;
         public int? Status { get; set; } = 0;
