a1d30d4386f3c4e6f84ee672711be422801a19a6
Admin <Admin@DESKTOP-40H717B>
2026-01-13 14:13:31 +0700

    Update entities

---
+++ RERPAPI.Model/Entities/EconomicContract.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RERPAPI.Model.Entities;
+
+/// <summary>
+/// Bảng lưu thông tin hợp đồng kinh tế
+/// </summary>
+public partial class EconomicContract
+{
+    public int ID { get; set; }
+
+    public int? STT { get; set; }
+
+    public int? EconomicContractTypeID { get; set; }
+
+    public int? EconomicContractTermID { get; set; }
+
+    public string? ContractNumber { get; set; }
+
+    public string? ContractContent { get; set; }
+
+    public string? NameNcc { get; set; }
+
+    public string? MSTNcc { get; set; }
+
+    public string? AddressNcc { get; set; }
+
+    public string? SDTNcc { get; set; }
+
+    public string? EmailNcc { get; set; }
+
+    public decimal? SignedAmount { get; set; }
+
+    public string? MoneyType { get; set; }
+
+    public string? TimeUnit { get; set; }
+
+    public string? Adjustment { get; set; }
+
+    public string? Note { get; set; }
+
+    public DateTime? SignDate { get; set; }
+
+    public DateTime? EffectDateFrom { get; set; }
+
+    public DateTime? EffectDateTo { get; set; }
+
+    public int? StatusContract { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    /// <summary>
+    /// 1 = NCC, 2 = KH
+    /// </summary>
+    public int? TypeNCC { get; set; }
+}
---
+++ RERPAPI.Model/Entities/EconomicContractTerm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RERPAPI.Model.Entities;
+
+public partial class EconomicContractTerm
+{
+    public int ID { get; set; }
+
+    public int? STT { get; set; }
+
+    public string? TermCode { get; set; }
+
+    public string? TermName { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public bool? IsDeleted { get; set; }
+}
