e2dc4b0bffea36473271f711447e9a23ccf83dac
Admin <Admin@DESKTOP-40H717B>
2026-01-14 11:04:47 +0700

    Update SqlDapper trả ra model

--- RERPAPI.Model/Common/SqlDapper.cs
+++ RERPAPI.Model/Common/SqlDapper.cs
@@ -10,7 +10,7 @@ using Dapper;
 
 namespace RERPAPI.Model.Common
 {
-    public static class SqlDapper
+    public static class SqlDapper<T> where T : class, new()
     {
         static string connectionString = Config.ConnectionString;
         static int commandTimeout = 2000;
@@ -25,6 +25,27 @@ namespace RERPAPI.Model.Common
                 //var menus = (await data.ReadAsync()).ToList();
                 //var userGroups = (await data.ReadAsync()).ToList();
                 var result = (await data.ReadAsync()).ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public static async Task<List<T>> ProcedureToListTAsync(string procedureName, object param)
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+                //var param = new { Keyword = keyword, UserID = _currentUser.ID };
+                var data = await connection.QueryMultipleAsync(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+
+                //var menus = (await data.ReadAsync()).ToList();
+                //var userGroups = (await data.ReadAsync()).ToList();
+                var result = (await data.ReadAsync<T>()).ToList();
                 return result;
             }
             catch (Exception ex)
