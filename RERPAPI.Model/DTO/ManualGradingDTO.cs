83a22c775edc4f2667a6fca66e66da95bfbe1871
Nhu Binh <Nhu Binh@DESKTOP-7O4JCTA>
2026-04-15 14:14:59 +0700

    thêm lưu file đính kèm chấm điểm, phân quyền lại

--- RERPAPI.Model/DTO/ManualGradingDTO.cs
+++ RERPAPI.Model/DTO/ManualGradingDTO.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+using RERPAPI.Model.Entities;
+
 namespace RERPAPI.Model.DTO
 {
     public class GradeEssayRequestDTO
     {
         public int ExamResultDetailID { get; set; }
         public decimal Score { get; set; }
+        public List<HRRecruitmentExamEvaluationFile>? EvaluationFiles { get; set; }
     }
 
     public class FinalizeGradingRequestDTO
