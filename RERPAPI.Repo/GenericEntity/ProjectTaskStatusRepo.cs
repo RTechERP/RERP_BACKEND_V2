e48ce6dd16332b87fb2e8ecc74660b82800bc36d
tunght2202 <hathanhtung2202@gmail.com>
2026-05-26 15:19:28 +0700

    update ProjectTaskV2.9 (Hiệu suất công việc + lấy danh sách nhân viên thiếu công việc)

---
+++ RERPAPI.Repo/GenericEntity/ProjectTaskStatusRepo.cs
@@ -0,0 +1,16 @@
+﻿using RERPAPI.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RERPAPI.Repo.GenericEntity
+{
+    public class ProjectTaskStatusRepo : GenericRepo<Model.Entities.ProjectTaskStatus>
+    {
+        public ProjectTaskStatusRepo(CurrentUser currentUser) : base(currentUser)
+        {
+        }
+    }
+}
