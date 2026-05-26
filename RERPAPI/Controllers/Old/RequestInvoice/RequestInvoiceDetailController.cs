0b5bd9fd2052cbb6611725928160431cf545c44a
tuanbeo <anhemnha8x@gmail.com>
2026-05-26 19:07:30 +0700

    update ẩn loại hợp đồng không dùng khi ycxhd, api xuất excel ls tiền về

--- RERPAPI/Controllers/Old/RequestInvoice/RequestInvoiceDetailController.cs
+++ RERPAPI/Controllers/Old/RequestInvoice/RequestInvoiceDetailController.cs
@@ -98,7 +98,7 @@ namespace RERPAPI.Controllers.Old.RequestInvoice
         {
             try
             {
-                var data = _accountingContractTypeRepo.GetAll().OrderByDescending(x => x.CreatedDate).ToList();
+                var data = _accountingContractTypeRepo.GetAll(x => x.IsHideRequestInvoice == false).OrderByDescending(x => x.CreatedDate).ToList();
                 return Ok(ApiResponseFactory.Success(data, ""));
             }
             catch (Exception ex)
