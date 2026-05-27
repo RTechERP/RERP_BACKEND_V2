f1d8205a21d71541f4b56a975cd3bc055f926c4d
HoangHai <ADMIN@DESKTOP-I8MS0CC>
2026-05-27 14:30:49 +0700

    Thêm api lưu & hiển thị log tài sản

--- RERPAPI.Repo/GenericEntity/Asset/TSAssetManagementRepo.cs
+++ RERPAPI.Repo/GenericEntity/Asset/TSAssetManagementRepo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,30 +43,32 @@ namespace RERPAPI.Repo.GenericEntity.Asset
         }
         public int GetMaxSTT()
         {
-            var data = GetAll();
-            if (!data.Any())
-                return 0;
-            return data.Max(x => x.STT) ?? 0;
+            return table.OrderByDescending(x => x.STT).Select(x => x.STT).FirstOrDefault() ?? 0;
         }
         public bool Validate(TSAssetManagement item, out string message)
         {
             message = "";
-            
-            bool exists = GetAll().Any(x => x.TSAssetCode == item.TSAssetCode && x.ID != item.ID && x.IsDeleted != true);
-            bool existSeri = GetAll().Any(x => x.Seri == item.Seri && x.ID != item.ID && x.IsDeleted != true);
-            bool existCodeNCC = GetAll().Any(x => x.TSCodeNCC == item.TSCodeNCC && x.ID != item.ID && x.IsDeleted != true);
-          
-            if (existSeri && !string.IsNullOrWhiteSpace(item.Seri))
+
+            if (!string.IsNullOrWhiteSpace(item.Seri) && table.Any(x => x.IsDeleted != true && x.ID != item.ID && x.Seri == item.Seri))
             {
                 message = $"Mã SerialNumber {item.Seri} đã tồn tại";
                 return false;
             }
-            if (existCodeNCC && !string.IsNullOrWhiteSpace(item.TSCodeNCC))
+
+            if (!string.IsNullOrWhiteSpace(item.TSCodeNCC) && table.Any(x => x.IsDeleted != true && x.ID != item.ID && x.TSCodeNCC == item.TSCodeNCC))
             {
                 message = $"Mã tài sản {item.TSCodeNCC} đã tồn tại";
                 return false;
             }
+
             return true;
+        }
+
+        public object GetExistingAssets(List<string> tsAssetCodes, List<string> tsCodeNCCs)
+        {
+            return table.Where(x => tsAssetCodes.Contains(x.TSAssetCode) && tsCodeNCCs.Contains(x.TSCodeNCC))
+                        .Select(x => new { x.ID, x.TSAssetCode, x.TSCodeNCC })
+                        .ToList();
         }
     }
 }
