596116136389db16cd2f4a35f9fd88d1d4910cc8
rtcnhat18 <tech61@rtc.edu.vn>
2026-06-13 08:26:07 +0700

    update lưu % giảm giá

---
+++ RERPAPI.Repo/GenericEntity/HistoryProductPriceRequestRepo.cs
@@ -0,0 +1,12 @@
+﻿using RERPAPI.Model.DTO;
+using RERPAPI.Model.Entities;
+
+namespace RERPAPI.Repo.GenericEntity
+{
+	public class HistoryProductPriceRequestRepo : GenericRepo<HistoryProductPriceRequest>
+	{
+		public HistoryProductPriceRequestRepo(CurrentUser currentUser) : base(currentUser)
+		{
+		}
+	}
+}
