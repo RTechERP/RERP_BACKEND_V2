cd116da111ef7a4510ec881d0c69bbd9e9ebbe02
Admin <Admin@DESKTOP-40H717B>
2026-07-20 09:27:23 +0700

    UPdate model

---
+++ RERPAPI.Model/Entities/InventoryStockLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RERPAPI.Model.Entities;
+
+public partial class InventoryStockLog
+{
+    public int ID { get; set; }
+
+    /// <summary>
+    /// InventoryStockID
+    /// </summary>
+    public int? InventoryStockID { get; set; }
+
+    /// <summary>
+    /// Loại log
+    /// </summary>
+    public string? TypeLog { get; set; }
+
+    /// <summary>
+    /// Nội dung log
+    /// </summary>
+    public string? ContentLog { get; set; }
+
+    /// <summary>
+    /// Ngày tạo
+    /// </summary>
+    public DateTime? CreatedDate { get; set; }
+
+    /// <summary>
+    /// người tạo
+    /// </summary>
+    public string? CreatedBy { get; set; }
+
+    /// <summary>
+    /// Trạng thái xóa
+    /// </summary>
+    public bool? IsDeleted { get; set; }
+}
