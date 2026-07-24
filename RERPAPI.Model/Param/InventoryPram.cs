4935d95d2524e1da7a399a902d0caeaa2963801c
Nam <vutunam.rtc@gmail.com>
2026-07-24 14:53:17 +0700

    Update chức năng in phiếu xuất nhập

--- RERPAPI.Model/Param/InventoryPram.cs
+++ RERPAPI.Model/Param/InventoryPram.cs
@@ -9,6 +9,7 @@
         public bool IsStock { get; set; }
         public DateTime? FromDate { get; set; }
         public DateTime? ToDate { get; set; }
+        public bool IsStandardized { get; set; }
         //public int PageSize { get; set; }
         //public int PageNumber { get; set; }
     }
